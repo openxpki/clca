0f47a2a7e63c02ae7054764a10f7a8356cba57f7
Martin Bartosch <m.bartosch@cynops.de>
2014-07-16 17:41:47 +0200

    change boot loader timeout

--- package/debian-live/addons.d/01-clca-version.sh
+++ package/debian-live/addons.d/01-clca-version.sh
@@ -1,6 +1,24 @@
+# boilerplates
+#
+TITLE="CLCA Live Root CD"
+BUILD_TIMESTAMP=`date +"%F %T%z"`
+GIT_COMMIT=`git rev-list --max-count=1 HEAD`
+GIT_DESCRIPTION=`git describe`
+
 # show clca version in /etc/clca_version
-echo "CLCA Live Root CD" >$TARGETROOT/etc/clca_version
-echo -n "Built from git commit " >>$TARGETROOT/etc/clca_version
-git rev-list --max-count=1 HEAD >>$TARGETROOT/etc/clca_version
-echo -n "Git description: " >>$TARGETROOT/etc/clca_version
-git describe >>$TARGETROOT/etc/clca_version
+cat <<EOF >$TARGETROOT/etc/clca_version
+$TITLE
+Built from git commit $GIT_COMMIT
+Git description: $GIT_DESCRIPTION
+EOF
+
+# customize boot screen
+mkdir -p config/includes/binary/isolinux
+
+cat <<EOF >config/includes/binary/isolinux/isolinux.cfg
+include menu.cfg
+default vesamenu.c32
+prompt 0
+timeout 80
+EOF
+
