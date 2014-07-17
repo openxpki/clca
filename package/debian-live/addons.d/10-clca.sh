7aac49c2d0373b7f695e67d5c234419b2543388a
Martin Bartosch <m.bartosch@cynops.de>
2014-07-17 22:58:01 +0200

    add pkcs#11, jre and bouncy castle

--- package/debian-live/addons.d/10-clca.sh
+++ package/debian-live/addons.d/10-clca.sh
@@ -3,11 +3,13 @@ PACKAGES="$PACKAGES openssl"
 
 # optional packages - useful for certain hardware tokens
 # smartcard support
-PACKAGES="$PACKAGES opensc libccid"
+PACKAGES="$PACKAGES opensc libccid libengine-pkcs11-openssl"
 # yubikey personalization
 PACKAGES="$PACKAGES yubikey-personalization"
 # gnupg-agent
 PACKAGES="$PACKAGES gnupg-agent"
+# java and bouncy castle
+PACKAGES="$PACKAGES default-jre-headless libbcprov-java"
 
 # files to install
 install -D -m 755 $BASE/bin/clca $TARGETROOT/usr/local/bin/clca
