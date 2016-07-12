1c3c0cdb3b665faa72c8aa7415127d820c737bc9
Martin Bartosch <m.bartosch@cynops.de>
2016-07-12 18:23:51 +0200

    modified infrastructure to work with Luna 6.2 drivers

--- package/debian-live/addons.d/05-luna-hsm.sh
+++ package/debian-live/addons.d/05-luna-hsm.sh
@@ -29,51 +29,11 @@ if [ -r "$LUNA_DIR/install.sh" ] ; then
     cp $LUNA_DIR/*.rpm $TARGETROOT/usr/safenet/SOFTWARE/
     cp $LUNA_DIR/common $TARGETROOT/usr/safenet/SOFTWARE/
     cp $LUNA_DIR/*.sh $TARGETROOT/usr/safenet/SOFTWARE/
+
     cp $LUNA_DIR/install.sh $TARGETROOT/usr/safenet/SOFTWARE/install-batch.sh
+    patch $TARGETROOT/usr/safenet/SOFTWARE/install-batch.sh < addons.d/install-batch.sh.patch
 
-cat <<EOF | patch $TARGETROOT/usr/safenet/SOFTWARE/install-batch.sh 
---- install-batch.sh.orig	2015-12-03 17:38:48.216660150 +0000
-+++ install-batch.sh	2015-12-03 18:44:45.916570680 +0000
-@@ -22,7 +22,7 @@
-
- # Used for Luna Products selection
- # By default no Luna Product will be installed
--LUNA_SA_SELECTED=" "
-+LUNA_SA_SELECTED="*"
- LUNA_PCI_SELECTED=" "
- LUNA_G5_SELECTED=" "
- LUNA_REM_BACKUP_HSM_SELECTED=" "
-@@ -52,6 +52,7 @@
- update_install=0
-
- prompt_yes_no() {
-+return 0
-     rsp=""
-     while [ "\$rsp" != "y" ] && [ "\$rsp" != "n" ] && [ "\$rsp" != "yes" ] && [ "\$rsp" != "no" ]
-     do
-@@ -1198,7 +1199,8 @@
-         fi
-     fi
-     prompt_for_products "\$option"
--    read option
-+    #read option
-+    option="n"
- done;
-
- # If no Luna products are installed then at least one must be selected in order to move to components selection
-@@ -1220,7 +1222,8 @@
-     fi
-
-     prompt_for_components "\$option"
--    read option
-+    #read option
-+    option="i"
- done;
- }
-
-
-EOF
-
+    chmod 755 $TARGETROOT/usr/safenet/SOFTWARE/*.sh
 
     mkdir -p $TARGETROOT/etc/profile.d/
     cat <<EOF >$TARGETROOT/etc/profile.d/luna-path.sh
