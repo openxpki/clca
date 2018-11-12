f62e23cad0994d69e7acfc8b30888e43a5ae59ec
Martin Bartosch <mbartosch@whiterabbitsecurity.com>
2018-11-12 22:59:05 +0100

    add provision tool to live cd build

--- package/debian-live/addons.d/10-clca.sh
+++ package/debian-live/addons.d/10-clca.sh
@@ -1,6 +1,9 @@
 # additional packages for this addon
 PACKAGES="$PACKAGES openssl"
 
+# required for provision tool
+PACKAGES="$PACKAGES libyaml-perl"
+
 # optional packages - useful for certain hardware tokens
 # smartcard support
 PACKAGES="$PACKAGES opensc libccid libengine-pkcs11-openssl"
@@ -13,5 +16,6 @@ PACKAGES="$PACKAGES default-jre-headless libbcprov-java"
 
 # files to install
 install -D -m 755 $BASE/bin/clca $TARGETROOT/usr/local/bin/clca
+install -D -m 755 $BASE/bin/provision $TARGETROOT/usr/local/bin/provision
 install -D -m 644 $BASE/etc/clca.cfg $TARGETROOT/usr/local/etc/clca.cfg
 install -D -m 644 $BASE/etc/openssl.cnf $TARGETROOT/usr/local/etc/openssl.cnf
